9c03c532d5eda91422a9beea553b23e8739b9255
Alex Studnička <AStudnicka@gmail.com>
2016-06-06 19:17:09 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "WebSocketClient",
     dependencies: [
-        .Package(url: "https://github.com/Zewo/WebSocket.git", majorVersion: 0, minor: 0),
+        .Package(url: "https://github.com/Zewo/WebSocket.git", majorVersion: 0, minor: 7),
         .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 6),
         .Package(url: "https://github.com/Zewo/Base64.git", majorVersion: 0, minor: 7),
         .Package(url: "https://github.com/VeniceX/HTTPClient.git", majorVersion: 0, minor: 6),
