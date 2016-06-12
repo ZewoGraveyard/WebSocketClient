c2afc593cfcf7988ec62adda1436e4f56c550e36
Dan Appel <dan.appel00@gmail.com>
2016-06-12 13:58:57 -0700

    Fix dependencies (c7 dependencies were inconsistent)

--- Package.swift
+++ Package.swift
@@ -3,10 +3,10 @@ import PackageDescription
 let package = Package(
     name: "WebSocketClient",
     dependencies: [
-        .Package(url: "https://github.com/Zewo/WebSocket.git", majorVersion: 0, minor: 7),
-        .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 6),
-        .Package(url: "https://github.com/Zewo/Base64.git", majorVersion: 0, minor: 7),
-        .Package(url: "https://github.com/VeniceX/HTTPClient.git", majorVersion: 0, minor: 6),
+        .Package(url: "https://github.com/Zewo/WebSocket.git", majorVersion: 0, minor: 8),
+        .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 7),
+        .Package(url: "https://github.com/Zewo/Base64.git", majorVersion: 0, minor: 8),
+        .Package(url: "https://github.com/VeniceX/HTTPClient.git", majorVersion: 0, minor: 7),
         .Package(url: "https://github.com/VeniceX/HTTPSClient.git", majorVersion: 0, minor: 7),
     ]
 )
